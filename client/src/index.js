80014d79ec794473c48de3f7c2e4691d31570ccf
Shin <shinoku1801@gmail.com>
2023-02-17 15:32:39 +0700

    re-deploy client site

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={Store}>
-            <h3>Change something</h3>
+            <App />
         </Provider>
     </React.StrictMode>,
 );
