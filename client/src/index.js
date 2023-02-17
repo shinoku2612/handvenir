81f9759f76d7de41ae96f68f5dbb252a89c74535
Shin <shinoku1801@gmail.com>
2023-02-17 15:19:59 +0700

    test vercel change

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={Store}>
-            <App />
+            <div>Hello world</div>
         </Provider>
     </React.StrictMode>,
 );
