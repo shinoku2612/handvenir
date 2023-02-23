45e041963e895ba0fedf8d2046076dac10862641
Shin <shinoku1801@gmail.com>
2023-02-23 16:28:45 +0700

    server: add cors deployed domain

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'https://shin-pay.vercel.app'],
         credentials: true,
     }),
 );
