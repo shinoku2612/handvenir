dede13e3f49513bb7513fed5448fbc284f983817
vanthong-iSoft <vanthong@i-soft.com.vn>
2023-12-06 14:26:44 +0700

    update

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:3000",
-            "http://localhost:3001",
+            "http://localhost:3003",
             "https://handvenir.vercel.app",
         ],
         credentials: true,
